2ab2bffc30f507c52d0e56933d97e9820b09b824
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-02 15:28:38 +0700

    Create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import userRouter from "./routes/user.route.js"
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=> {
@@ -18,3 +19,5 @@ app.listen(3000, () => {
 app.get('/', function(req,res){
 	res.send("Hallo")
 })
+
+app.use("/api/users/", userRouter)
