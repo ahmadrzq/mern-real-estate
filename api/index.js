e2b48b2f0866899c8dd6252d59dd53678648d4ca
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-02 16:11:55 +0700

    Create register api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(()=> {
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGO).then(()=> {
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(3000, () => {
 	console.log("Server running on http://localhost:3000")
 })
@@ -20,4 +23,5 @@ app.get('/', function(req,res){
 	res.send("Hallo")
 })
 
-app.use("/api/users/", userRouter)
+app.use("/api/users", userRouter)
+app.use("/api/auth", authRouter)
