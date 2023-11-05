4eec0297909b4ed08b5f95504e8bf8ef01ad85c3
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-11-05 10:19:42 +0700

    Complete login page functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,23 +1,67 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 
 export default function Login() {
+    const [formData, setFormData] = useState({})
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            setLoading(true)
+            const res = await fetch("/api/auth/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData),
+            })
+            const data = await res.json()
+            if (data.success === false) {
+                setError(data.message)
+                setLoading(false)
+                return
+            }
+            setLoading(false)
+            setError(null)
+            navigate('/')
+        } catch (error) {
+            setLoading(false)
+            setError(data.message)
+        }
+    }
+    // console.log(formData)
     return (
         <>
             <div className="p-3 max-w-lg mx-auto">
                 <h1 className="text-3xl text-center font-semibold my-7">
                     Login
                 </h1>
-                <form className="flex flex-col gap-4">
-                    <input type="email" name="" id="email" placeholder="Email" className="border outline-none p-3 rounded-lg" required/>
-                    <input type="password" name="" id="password" placeholder="Password" className="border outline-none p-3 rounded-lg" required/>
-                    <button className="bg-[#274C77] text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-60">Login</button>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                    <input type="email" name="" id="email" placeholder="Email" className="border outline-none p-3 rounded-lg" onChange={handleChange} />
+                    <input type="password" name="" id="password" placeholder="Password" className="border outline-none p-3 rounded-lg" onChange={handleChange} />
+                    <button disabled={loading} className="bg-[#274C77] text-white p-3 rounded-lg uppercase hover:opacity-80 disabled:opacity-60">
+                        {
+                            loading ? 'Loading...' : 'Login'
+                        }
+                    </button>
                 </form>
                 <div className="flex gap-2 mt-5">
-                    <p>Don't have an account?</p>
+                    <p>Dont have an account?</p>
                     <Link to={"/register"}>
                         <span className="text-blue-700">Register</span>
                     </Link>
                 </div>
+                {error && <p className="text-red-600 mt-5">{error}</p>}
             </div>
         </>
     )
